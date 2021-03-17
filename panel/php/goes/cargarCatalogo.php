7a068dfd4b64c0b82accf191e982df547cf323a6
Daniel Cortes <domcc.88@icloud.com>
2021-03-17 16:07:46 -0300

    adding ajax to charge catalogue

--- panel/php/goes/cargarCatalogo.php
+++ panel/php/goes/cargarCatalogo.php
@@ -25,15 +25,15 @@
 		                </thead>
 		                <tbody>";
 		        // foreach ($productos as $producto) {
-			       //  $tabla.="
-			       //      <tr>
-			       //          <td><p class='text-center'>".$producto['nombreCatalogo']."</p></td>
-			       //          <td><img class='img-thumbnail' src='".$producto['imagen']."' alt='' width='50%' height='40%'></td>
-			       //          <td>".$producto['sato']."</td>
-			       //          <td>".$producto['nombre']."</td>
-			       //          <td>".$producto['descripcion']."</td>
-			       //      	<td>".$producto['precioVenta']."</td>
-			       //      </tr>";
+			        $tabla.="
+			            <tr>
+			                <td><p class='text-center'>".$producto['nombreCatalogo']."</p></td>
+			                <td><img class='img-thumbnail' src='".$producto['imagen']."' alt='' width='50%' height='40%'></td>
+			                <td>".$producto['sato']."</td>
+			                <td>".$producto['nombre']."</td>
+			                <td>".$producto['descripcion']."</td>
+			            	<td>".$producto['precioVenta']."</td>
+			            </tr>";
 		        // }
 
 		        $tabla.="</tbody>
