cda5285468755c1ea3ba65032700b1bb44cbdf3d
Daniel Cortes <domcc.88@icloud.com>
2021-03-17 16:08:49 -0300

    adding ajax to charge catalogue

--- panel/php/goes/cargarCatalogo.php
+++ panel/php/goes/cargarCatalogo.php
@@ -1,64 +1,49 @@
 <?php
 	if (isset($_POST['idCatalogo'])) {
-		// require_once("../clases/cc2.php");
-		// $lista = new metodos();
-		// $sqlProductos =
-		// 	"SELECT productosCatalogo.id_catalogo,catalogo.nombreCatalogo AS nombreCatalogo,productosCatalogo.imagen,productosCatalogo.sato,productosCatalogo.nombre,productosCatalogo.descripcion,productosCatalogo.precioVenta FROM productosCatalogo INNER JOIN catalogo ON productosCatalogo.id_catalogo = catalogo.idCatalogo ORDER BY id";
-		// try {
-			$productos = $lista->mostrarDatos($sqlProductos);
-			$tabla = "
-				<div class='card card-primary'>
-		          <div class='card-header'>
-		            <h3 class='card-title'>LISTADO DE PRODUCTO DE CATALOGO</h3>
-		          </div>
-		            <div class='card-body'>
-		              <table id='example1' class='table table-bordered table-striped'>
-		                <thead>
-		                  <tr>
-		                    <th>Nombre catalogo</th>
-		                    <th>Foto</th>
-		                    <th>Sato</th>
-		                    <th>Nombre</th>
-		                    <th>Descripcion</th>
-		                    <th>Precio Venta</th>
-		                  </tr>
-		                </thead>
-		                <tbody>";
-		        // foreach ($productos as $producto) {
-			        $tabla.="
-			            <tr>
-			                <td><p class='text-center'>".$producto['nombreCatalogo']."</p></td>
-			                <td><img class='img-thumbnail' src='".$producto['imagen']."' alt='' width='50%' height='40%'></td>
-			                <td>".$producto['sato']."</td>
-			                <td>".$producto['nombre']."</td>
-			                <td>".$producto['descripcion']."</td>
-			            	<td>".$producto['precioVenta']."</td>
-			            </tr>";
-		        // }
+		$tabla = "<div class='card card-primary'>
+          <div class='card-header'>
+            <h3 class='card-title'>LISTADO DE PRODUCTO DE CATALOGO</h3>
+          </div>
+            <div class='card-body'>
+              <table id='example1' class='table table-bordered table-striped'>
+                <thead>
+                  <tr>
+                    <th>Nombre catalogo</th>
+                    <th>Foto</th>
+                    <th>Sato</th>
+                    <th>Nombre</th>
+                    <th>Descripcion</th>
+                    <th>Precio Venta</th>
+                  </tr>
+                </thead>
+                <tbody>";
 
-		        $tabla.="</tbody>
-		                <tfoot>
-		                  <tr>
-		                    <th>Nombre catalogo</th>
-		                    <th>Foto</th>
-		                    <th>Sato</th>
-		                    <th>Nombre</th>
-		                    <th>Descripcion</th>
-		                    <th>Precio Venta</th>
-		                  </tr>
-		                </tfoot>
-		              </table>
-		            </div>
-		          </div>
-		      </div>";
-		    echo $tabla;
-	// 	} catch (Exception $e) {
-	// 		echo "excepcion ".$e;
-	// 	}
-	// } else {
-	// 	echo "sin resultados";
+        $tabla.="
+	                <tr>
+	                    <td><p class='text-center'>nombre catalogo</p></td>
+	                    <td><img class='img-thumbnail' src='' alt='' width='50%' height='40%'></td>
+	                    <td>sato</td>
+	                    <td>nombre</td>
+	                    <td>descripcion del producto</td>
+                    <td>precio venta</td>
+	                </tr>";
+        $tabla.="</tbody>
+                <tfoot>
+                  <tr>
+                    <th>Nombre catalogo</th>
+                    <th>Foto</th>
+                    <th>Sato</th>
+                    <th>Nombre</th>
+                    <th>Descripcion</th>
+                    <th>Precio Venta</th>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+      </div>";
 	}
 
-
+	echo $tabla;
 
 ?>
