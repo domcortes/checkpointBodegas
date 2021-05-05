d1e1e192afbf1e8fbaa36a35777762daa104e322
Daniel Cortes <domcc.88@icloud.com>
2021-05-05 11:52:39 -0300

    add button to export listadoLink

--- panel/pages/tables/televenta/listadoLinks.php
+++ panel/pages/tables/televenta/listadoLinks.php
@@ -212,7 +212,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Listado de links</h1>
+            <h1>Listado de links</h1> <input type="button" class="btn-primary" name="exportarxls" id="exportarxls" value="Exportar Excel" disabled>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
